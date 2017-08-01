ffd5f8d83ae3fd89d2cffbbee127778e05648ed6
Dawid <dawidbasa@gmail.com>
2017-08-02 00:47:52 +0200

    Impoved sysOuts to better readability, added thenCompare(Name) comparator to sortEmployessBySalary now if employees have same salary method sort by name.

--- src/main/java/pl/dawidbasa/EmployeSalarySorting/App.java
+++ src/main/java/pl/dawidbasa/EmployeSalarySorting/App.java
@@ -9,43 +9,16 @@ import java.util.stream.Collectors;
 
 public class App {
 
-	public static void main(String[] args) {
-
-		List<Employee> employess = Arrays.asList(
-
-				new Employee("Dawid", 3000), 
-				new Employee("Roman", 5942), 
-				new Employee("Tomasz", 2345),
-				new Employee("Marcin", 4730),
-				new Employee("Adrian", 1999), 
-				new Employee("Mariusz", 1234),
-				new Employee("Micha³", 8503), 
-				new Employee("Kamil", 12000));
-
-		// 1. Sort Staff by name
-		System.out.println(App.sortEmployessByName(employess));
-
-		// 2. Sort Staff by salary
-		System.out.println(App.sortEmployessBySalary(employess));
-
-		// 3. Calculate average salary
-		System.out.println(App.calculateAverageSalary(employess));
-
-		// 4. Get Staff with sallary bigger than average salary.
-		System.out.println(filterEmployees(employess, isSalaryBiggerThanAverage(App.calculateAverageSalary(employess))));
-
+	public static List<Employee> sortEmployeesByName(List<Employee> Employees){
+		Collections.sort(Employees, (e1, e2) -> e1.getName().compareTo(e2.getName()));
+		return Employees;
 	}
 	
-	public static List<Employee> sortEmployessByName(List<Employee> employess){
-		Collections.sort(employess, (e1, e2) -> e1.getName().compareTo(e2.getName()));
-		return employess;
-	}
-	
-	public static List<Employee> sortEmployessBySalary(List<Employee> employess){
-		employess.sort
+	public static List<Employee> sortEmployeesBySalary(List<Employee> Employees){
+		Employees.sort
 			(Comparator.comparingDouble(Employee::getSalary)
-					.reversed());
-		return employess;
+					.reversed().thenComparing((e1, e2) -> e1.getName().compareTo(e2.getName())));
+		return Employees;
 		
 	}
 
@@ -59,11 +32,41 @@ public class App {
 		return e -> e.getSalary() > averageSalary;
 	}
 	
-	public static double calculateAverageSalary(List<Employee> employess){
-		return  employess.stream()
+	public static double calculateAverageSalary(List<Employee> Employees){
+		return  Employees.stream()
 				.mapToDouble(Employee::getSalary)
 				.average()
 				.getAsDouble();
 	}
+	
+	
+	public static void main(String[] args) {
+
+		List<Employee> Employees = Arrays.asList(
+
+				new Employee("Dawid", 3000), 
+				new Employee("Roman", 3000), 
+				new Employee("Tomasz", 3000),
+				new Employee("Marcin", 4730),
+				new Employee("Adrian", 1999), 
+				new Employee("Mariusz", 1234),
+				new Employee("Micha³", 8503), 
+				new Employee("Kamil", 12000));
+
+		// 1. Sort Staff by name
+		System.out.println("Sort By name " + App.sortEmployeesByName(Employees));
+
+		// 2. Sort Staff by salary
+		System.out.println("Sort By salary " + App.sortEmployeesBySalary(Employees));
+
+		// 3. Calculate average salary
+		System.out.println("Average Salary " + App.calculateAverageSalary(Employees));
+
+		// 4. Get Staff with sallary bigger than average salary.
+		System.out.println("Staff with sallary bigger than average " +filterEmployees(Employees, isSalaryBiggerThanAverage(App.calculateAverageSalary(Employees))));
+
+	}
+	
+	
 
 }
